cbcec28e5bf59b847efb4706cfc9171db12cdd5a
unknown <xiagq@gildata.com>
2018-06-04 20:08:38 +0800

    webpack 4.x all in one

---
+++ webpack-4-all-in-one/readme.md
@@ -0,0 +1,19 @@
+# webpack-4-all-in-one
+
+
+```sh
+
+$ mkdir webpack-4-all-in-one && cd $_
+
+$ npm i -S webpack
+
+$ npm i -D webpack-cli
+
+```
+
+
+
+
+
+
+
---
+++ webpack-4-all-in-one/src/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright gildata
+ *
+ * @description webpack 4.x all in one
+ * @augments
+ * @example
+ *
+ */
+const Generator = (datas = [], debug = false) => {
+    let result = ``;
+    // do something...
+    console.log(`ES6 is petty cool!`);
+    return result;
+};
